fe0c7ee7153372fdddc59489bddfb3a88b263325
s1260188 <s1260188@u-aizu.ac.jp>
2019-07-08 17:32:15 +0900

    first Dicegame

---
+++ DiceGame.py
@@ -0,0 +1,30 @@
+import random
+
+class Die:
+    def __init__(self):
+        self._faceValue=random.randint(1,6)
+    def GetFacevalue(self):
+        return self._faceValue
+    
+class DiceGame:
+    def __init__(self):
+ #       print("What is your name?")
+ #       name=input(">")
+        sum=0
+        print("Rolling the dice...")
+        for i in range(2):
+            d=Die()
+            v=d.GetFacevalue()
+            sum+=v
+            print("Die {0}: {1}".format(i+1,v))
+        print("Total value: {0}".format(sum))
+ #       if sum>=7:
+ #           print("You won!")
+ #           print("{0} won!".format(name))
+ #       else:
+ #           print("You lose")
+ #           print("{0} lost".format(name))
+
+if __name__=="__main__":
+    DiceGame()
+        
