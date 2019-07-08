4273d34b048d13f5d82ec1965cfcb218e3d55be3
s1260188 <s1260188@u-aizu.ac.jp>
2019-07-08 17:57:35 +0900

    final version

--- DiceGame.py
+++ DiceGame.py
@@ -8,6 +8,9 @@ class Die:
     
 class DiceGame:
     def __init__(self):
+        print("What is your name?")
+        name=input(">")
+        print("Hello,{0}!".format(name,))
         sum=0
         print("Rolling the dice...")
         for i in range(2):
@@ -17,11 +20,11 @@ class DiceGame:
             print("Die {0}: {1}".format(i+1,v))
         print("Total value: {0}".format(sum))
         if sum>=7:
-            print("You won!")
- #           print("{0} won!".format(name))
+#           print("You won!")
+            print("{0} won!".format(name))
         else:
-            print("You lose")
- #           print("{0} lost".format(name))
+#           print("You lose")
+            print("{0} lost".format(name))
 
 if __name__=="__main__":
     DiceGame()
